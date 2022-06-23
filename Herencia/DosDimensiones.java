19db895b6ab119301e593b7b129491ad8b98c946
Pieralessandro Garcia <103224509+pier1107@users.noreply.github.com>
2022-06-23 08:10:51 -0500

    Update DosDimensiones.java

--- Herencia/DosDimensiones.java
+++ Herencia/DosDimensiones.java
@@ -1,11 +1,20 @@
 public class DosDimensiones{
-    private double base;
-    private double altura;
+    protected double base;
+    protected double altura;
+
     //Métodos de acceso para base y altura
-    double getBase(){return base;}
-    double getAltura(){return altura;}
-    void setBase(double b){base=b;}
-    void setAltura (double h){altura=h;}
+    double getBase(){
+        return base;
+        }
+    double getAltura(){
+        return altura;
+        }
+    public void setBase(double b){
+        base=b;
+        }
+    public void setAltura (double h){
+        altura=h;
+        }
     void mostrarDimension(){
         System.out.println("La base y altura es: "+base+" y "+altura);
     }
